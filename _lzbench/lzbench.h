bb658729be9600309d9e5c3c41de0ae103381236
Przemyslaw Skibinski <inikep@gmail.com>
2016-11-16 17:45:03 +0100

    reordered Travis tests

--- _lzbench/lzbench.h
+++ _lzbench/lzbench.h
@@ -207,11 +207,6 @@ static const compressor_desc_t comp_desc[LZBENCH_COMPRESSOR_COUNT] =
 };
 
 
-#if defined(__arm__) || defined(__aarch64__)
-    #define BROTLI_LEVELS "brotli,0,2,5,8/"
-#else
-    #define BROTLI_LEVELS "brotli,0,2,5,8,11/"
-#endif
 
 #define LZBENCH_ALIASES_COUNT 12
 
@@ -219,12 +214,21 @@ static const alias_desc_t alias_desc[LZBENCH_ALIASES_COUNT] =
 {
     { "fast", "density/fastlz/lz4/lz4fast,3,17/lz5,1,2,3,4,5/lzf/lzfse/lzjb/lzo1b,1/lzo1c,1/lzo1f,1/lzo1x,1/lzo1y,1/" \
               "lzrw,1,2,3,4,5/lzsse4fast/lzsse8fast/lzvn/pithy,0,3,6,9/quicklz,1,2/shrinker/snappy/tornado,1,2,3/zstd,1,2,3,4,5" }, // default alias
-    { "all",  "blosclz,1,3,6,9/brieflz/" BROTLI_LEVELS \
+#if !defined(__arm__) && !defined(__aarch64__)
+    { "all",  "blosclz,1,3,6,9/brieflz/brotli,0,2,5,8,11/" \
               "crush,0,1,2/csc,1,3,5/density,1,2,3/fastlz,1,2/gipfeli/libdeflate,1,3,6,9,12/lz4/lz4fast,3,17/lz4hc,1,4,9,12,16/lz5,1,3,6,9,10,12,15,18/lz5huf,1,3,6,9,10,12,15,18/" \
               "lzf,0,1/lzfse/lzg,1,4,6,8/lzham,0,1/lzjb/lzlib,0,3,6,9/lzma,0,2,4,5/lzo1/lzo1a/lzo1b,1,3,6,9,99,999/lzo1c,1,3,6,9,99,999/lzo1f/lzo1x/lzo1y/lzo1z/lzo2a/" \
               "lzrw,1,2,3,4,5/lzsse2,1,6,12,16/lzsse4,1,6,12,16/lzsse8,1,6,12,16/lzvn/pithy,0,3,6,9/quicklz,1,2,3/snappy/slz_zlib/tornado,1,2,3,4,5,6,7,10,13,16/" \
               "ucl_nrv2b,1,6,9/ucl_nrv2d,1,6,9/ucl_nrv2e,1,6,9/xpack,1,6,9/xz,0,3,6,9/yalz77,1,4,8,12/yappy,1,10,100/zlib,1,6,9/zling,0,1,2,3,4/zstd,1,2,5,8,11,15,18,22/" \
               "shrinker/wflz/lzmat" }, // these can SEGFAULT
+#else
+    { "all",  "blosclz,1,3,6,9/brieflz/brotli,0,2,5,8/" \
+              "crush,0,1,2/csc,1,3,5/density,1,2,3/fastlz,1,2/gipfeli/libdeflate,1,3,6,9,12/lz4/lz4fast,3,17/lz4hc,1,4,9,12/lz5,1,3,6,10,12,15/lz5huf,1,3,6,10,12,15/" \
+              "lzf,0,1/lzfse/lzg,1,4,6,8/lzham,0,1/lzjb/lzlib,0,3,6,9/lzma,0,2,4,5/lzo1/lzo1a/lzo1b,1,3,6,9,99,999/lzo1c,1,3,6,9,99,999/lzo1f/lzo1x/lzo1y/lzo1z/lzo2a/" \
+              "lzrw,1,2,3,4,5/lzsse2,1,6,12,16/lzsse4,1,6,12,16/lzsse8,1,6,12,16/lzvn/pithy,0,3,6,9/quicklz,1,2,3/snappy/slz_zlib/tornado,1,2,3,4,5,6,7,10,13,16/" \
+              "ucl_nrv2b,1,6,9/ucl_nrv2d,1,6,9/ucl_nrv2e,1,6,9/xpack,1,6,9/xz,0,3,6,9/yalz77,1,4,8,12/yappy,1,10,100/zlib,1,6,9/zling,0,1,2,3,4/zstd,1,2,5,8,11,15,18,22/" \
+              "shrinker/wflz/lzmat" }, // these can SEGFAULT
+#endif
     { "opt",  "brotli,6,7,8,9,10,11/csc,1,2,3,4,5/lzham,0,1,2,3,4/lzlib,0,1,2,3,4,5,6,7,8,9/lzma,0,1,2,3,4,5,6,7,8,9/" \
               "tornado,5,6,7,8,9,10,11,12,13,14,15,16/xz,1,2,3,4,5,6,7,8,9/zstd,18,19,20,21,22" },
     { "lzo1",  "lzo1,1,99" },
