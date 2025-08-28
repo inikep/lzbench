fc659bd0440a04393a4e90b1d19e3c8f19ecdbae
Guillaume <3772944+g1mv@users.noreply.github.com>
2025-08-28 02:47:44 +0200

    Revert "Include cleanup"
    This reverts commit 5b03ad614724e07699c115448de12ecb2abe8456.

--- bench/symmetric_codecs.cpp
+++ bench/symmetric_codecs.cpp
@@ -325,7 +325,10 @@ int64_t lzbench_zpaq_decompress(char *inbuf, size_t insize, char *outbuf, size_t
 
 
 #ifndef BENCH_REMOVE_DENSITY
-#include "misc/density/density.h"
+extern "C"
+{
+    #include "misc/density/density.h"
+}
 
 char* lzbench_density_init(size_t insize, size_t level, size_t)
 {
